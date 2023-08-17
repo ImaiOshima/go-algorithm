40b59ac53e417a7da6746fd7c3558a0dfb0c8d86
imaiOshima <616357796@qq.com>
2023-08-17 21:50:47 +0800

    Initial commit

---
+++ algorithm/leetcode_3.go
@@ -0,0 +1,25 @@
+package main
+
+func lengthOfLongestSubstring(s string) int {
+	var max, l int
+	cnt := [128]int{}
+	for r, c := range s {
+		cnt[c]++
+		// 注意 这个是for循环 不是if条件 大于2说明有重复元素
+		for cnt[c] > 1 {
+			// for循环 从老的左边界开始排查 到底是那个超过了2 因为重复元素是超过2的
+			cnt[s[l]]--
+			l++
+		}
+		max = Max(max, r-l+1)
+	}
+	return max
+}
+
+func Max(a, b int) int {
+	if a >= b {
+		return a
+	} else {
+		return b
+	}
+}
