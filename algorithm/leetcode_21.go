34ffa31cc033ea5f3e2b7098dd30330487c6b498
imaiOshima <616357796@qq.com>
2023-08-30 21:58:38 +0800

    21. 合并两个有序链表

---
+++ algorithm/leetcode_21.go
@@ -0,0 +1,19 @@
+package main
+
+/**
+21. 合并两个有序链表
+*/
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
+	} else if list2 == nil {
+		return list1
+	} else if list1.Val <= list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
