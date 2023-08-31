d8651e25d9edcf72e567db0986a6e862118b54bf
imaiOshima <616357796@qq.com>
2023-08-31 23:52:53 +0800

    102. 二叉树的层序遍历 层序遍历 是树当中比较简单的算法题

---
+++ algorithm/leetcode_102.go
@@ -0,0 +1,31 @@
+package main
+
+/**
+102. 二叉树的层序遍历
+*/
+
+func levelOrder(root *TreeNode) [][]int {
+	var ans [][]int
+	if root == nil {
+		return ans
+	}
+	list := []*TreeNode{root}
+	for len(list) != 0 {
+		size := len(list)
+		vals := make([]int, size)
+		for i := range vals {
+			node := list[0]
+			list = list[1:]
+			vals[i] = node.Val
+
+			if node.Left != nil {
+				list = append(list, node.Left)
+			}
+			if node.Right != nil {
+				list = append(list, node.Right)
+			}
+		}
+		ans = append(ans, vals)
+	}
+	return ans
+}
